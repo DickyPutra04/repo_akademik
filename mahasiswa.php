89f624b2279443e85b82da2bffc54c0e573f34fa
Dicky Permana <dickyanime04@gmail.com>
2025-03-10 12:00:03 +0700

    Update mahasiswa.php
    Perubahan Data Mahasiswa

--- mahasiswa.php
+++ mahasiswa.php
@@ -7,6 +7,9 @@
         <hr>
         <h6>NIM: 111111</h6><br>
         <h6>Nama: Dicky Permana Putra</h6><br>
+        <h6>Ketua Kelas</h6><br>
+        <h6>NIM : 23110029</h6><br>
+        <h6>Brilian Ricky</h6>
         <a href="index.php">Kembali</a>
     </body>
 </html>
